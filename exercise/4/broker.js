6dcf7f4455c54796ca6b5a30ce0bb1f972868f25
Tinson Lai <laitingsheng@hotmail.com>
2020-09-24 22:51:33 +0930

    Completed prac4 module with incomplete test

--- exercise/4/broker.js
+++ exercise/4/broker.js
@@ -1,3 +1,13 @@
-record = {}
+const record = {}
 
-exports.emit = (type) => {}
+exports.emit = (type) => {
+    record[type].forEach((_, handler) => { handler() })
+}
+
+exports.subscribe = (type, handler) => {
+    record[type] = (record[type] ?? new Map()).set(handler, true)
+}
+
+exports.unsubscribe = (type, handler) => {
+    record[type].delete(handler)
+}
