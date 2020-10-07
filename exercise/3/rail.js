68a652edb181d6bbef93e8d3f10176076843000f
Tinson Lai <laitingsheng@hotmail.com>
2020-10-07 18:36:52 +1030

    Simplify rail.js

--- exercise/3/rail.js
+++ exercise/3/rail.js
@@ -1,55 +1,50 @@
-const _marking = {}
-const _str = {}
+const _marking = {};
+const _str = {};
 
-const _directions = {
+const _directions = Object.freeze({
     e: "East",
     w: "West",
     East: "e",
     West: "w"
-}
+});
 
-Object.freeze(_directions)
-
-const _record = {}
+const _record = {};
 
 for (const i of [1, 4, 8]){
-    const di = `d${i}`
-    _marking[di] = 1
-    _str[di] = di
+    const di = `d${i}`;
+    _marking[di] = 1;
+    _str[di] = di;
 }
 
 for (const i of [3, 9]){
-    const oi = `o${i}`
-    _marking[oi] = 1
-    _str[oi] = oi
-}
-
-function* _range(start, end) {
-    for (i = start; i < end; ++i)
-        yield i
+    const oi = `o${i}`;
+    _marking[oi] = 1;
+    _str[oi] = oi;
 }
 
 // some states are useless, this is just a shorter initialisation
-for (const i of _range(1, 14)) {
-    const pi =`p${i}`
-    _marking[pi] = 1
-    _str[pi] = pi
-    const pi_e = `p${i}_e`
-    _marking[pi_e] = 0
-    _str[pi_e] = pi_e
-    const pi_w = `p${i}_w`
-    _marking[pi_w] = 0
-    _str[pi_w] = pi_w
+for (i = 1; i <= 13; ++i) {
+    const pi =`p${i}`;
+    _marking[pi] = 1;
+    _str[pi] = pi;
 
-    _record[i] = undefined
+    const pi_e = `p${i}_e`;
+    _marking[pi_e] = 0;
+    _str[pi_e] = pi_e;
+
+    const pi_w = `p${i}_w`;
+    _marking[pi_w] = 0;
+    _str[pi_w] = pi_w;
+
+    _record[i] = null;
 }
 
-Object.seal(_marking)
-Object.freeze(_str)
-Object.seal(_record)
+Object.seal(_marking);
+Object.freeze(_str);
+Object.seal(_record);
 
 // This is a collection of all possible transitions in the Petri Net as said in the plan
-const _transitions = {
+const _transitions = Object.freeze({
     d1: [
         [[_str.d1, _str.p1], [_str.d1, _str.p1_e]]
     ],
@@ -90,7 +85,6 @@ const _transitions = {
     p10_w: [
         [[_str.p10_w, _str.p9], [_str.p10, _str.p9_w]]
     ],
-
     p5_e: [
         [[_str.p5_e, _str.p6], [_str.p5, _str.p6_e]],
         [[_str.p5_e, _str.p11], [_str.p5, _str.p11_e]]
@@ -150,84 +144,62 @@ const _transitions = {
         [[_str.p12_w, _str.p11, _str.p6], [_str.p12, _str.p11_w, _str.p6]],
         [[_str.p12_w, _str.p11, _str.p6_e], [_str.p12, _str.p11_w, _str.p6_e]]
     ]
-}
+});
 
-Object.freeze(_transitions)
-
-const _precheck = pre => _marking[pre] === 1
+const _precheck = pre => _marking[pre] === 1;
 
 const _trigger = state => {
     for (const [pres, posts] of _transitions[state])
         if (pres.every(_precheck)) {
             for (const pre of pres)
-                _marking[pre] = 0
+                _marking[pre] = 0;
             for (const post of posts)
-                _marking[post] = 1
-            return posts[1]
+                _marking[post] = 1;
+            return posts[1];
         }
-}
+};
 
-const _re = /^([dop])(\d{1,2})(?:_([ew]))?$/
+const _re = /^([dop])(\d{1,2})(?:_([ew]))?$/;
 
 const _parse = ss => {
-    const [, place, track, direction] = ss.match(_re)
-    return [place, parseInt(track, 10), _directions[direction]]
+    const [, place, track, direction] = ss.match(_re);
+    return [place, parseInt(track, 10), _directions[direction]];
 }
 
-const _filter = i => i !== undefined
+const _filter = i => i !== null;
 
-const _render = () => updateYard(Object.values(_record).filter(_filter))
+const _render = () => updateYard(Object.values(_record).filter(_filter));
 
-var _count = 1
+var _count = 1;
 
 const _new = i => {
-    if (_record[i] === undefined) {
-        const [place, track, direction] = _parse(_trigger(`d${i}`))
-        if (place == "p") {
-            _record[i] = {
-                name: `Train${_count++}`,
-                direction: direction,
-                blockSection: track
-            }
-            _render()
-        } else {
-            alert("impossible movement, the marking is now corrupted, refreshing the page")
-            location.reload()
-        }
+    if (_record[i] === null) {
+        const [, track, direction] = _parse(_trigger(`d${i}`));
+        _record[i] = {
+            name: `Train${_count++}`,
+            direction: direction,
+            blockSection: track
+        };
+        _render();
     } else
-        alert(`Track ${i} already has a train`)
+        alert(`Track ${i} already has a train`);
 }
 
 for (const [d, i] of [[_directions.e, 1], [_directions.e, 4], [_directions.w, 8]])
-    window[`newTrain${d}${i}`] = () => _new(i)
+    window[`newTrain${d}${i}`] = () => _new(i);
 
 const trainMove = train => {
-    const {blockSection, direction} = train
-    if (_record[blockSection] === undefined)
-        alert(`Unexpected train ${JSON.stringify(train)}`)
+    const { blockSection, direction } = train;
+    const re = _trigger(`p${blockSection}_${_directions[direction]}`);
+    if (re === undefined)
+        alert(`Train ${JSON.stringify(train)} cannot move`);
     else {
-        const re = _trigger(`p${blockSection}_${_directions[direction]}`)
-        if (re === undefined)
-            alert(`Train ${JSON.stringify(train)} cannot move`)
-        else {
-            const [place, track, new_direction] = _parse(re)
-            if (place == "d") {
-                alert("impossible movement, the marking is now corrupted, refreshing the page")
-                location.reload()
-            } else if (place == "o") {
-                _record[blockSection] = undefined
-                _render()
-            } else if (new_direction != direction || _record[track] !== undefined) {
-                alert("inconsistent result, the marking is now corrupted, refreshing the page")
-                location.reload()
-            } else {
-                _record[blockSection] = undefined
-                if (place == "p") {
-                    train.blockSection = track
-                    _record[track] = train
-                }
-                _render()
-            }
+        const [place, track] = _parse(re);
+        _record[blockSection] = null;
+        if (place == "p") {
+            train.blockSection = track;
+            _record[track] = train;
         }
+        _render();
     }
-}
+};
