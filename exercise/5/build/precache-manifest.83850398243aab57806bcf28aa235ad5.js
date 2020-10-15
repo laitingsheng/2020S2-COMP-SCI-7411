2092be4a949dbbf0753667a615d3fa94b8cd49d7
Tinson Lai <laitingsheng@hotmail.com>
2020-10-15 22:21:36 +1030

    Update Exercise 5 & Project

---
+++ exercise/5/build/precache-manifest.83850398243aab57806bcf28aa235ad5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89fae38d12d805d6486ba94549f3cde4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8314f5951457cd4cbeef",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ddb692841679c8d6411a",
+    "url": "/static/js/2.94e324dd.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.94e324dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8314f5951457cd4cbeef",
+    "url": "/static/js/main.8108e214.chunk.js"
+  },
+  {
+    "revision": "9119ce8e15860fabe7a8",
+    "url": "/static/js/runtime-main.0bec7f43.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
